eb3ccd808cbcab433f814d78f456e1c3c5c945e1
Hournet <bumblebee2911@gmail.com>
2023-07-01 16:39:38 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+document.addEventListener("DOMContentLoaded", ready);
+
+var xPosition = 0;
+var yPosition = 0;
+
+function handleMotionEvent(event) {
+  let card = document.getElementById("card");
+  
+  if (!flag) {
+    card.classList.remove("heartBeat");
+  }
+  var accelerationX = event.acceleration.x;
+  var accelerationY = event.acceleration.y;
+  var rotationRateBeta = event.rotationRate.beta;
+  var rotationRateGamma = event.rotationRate.alpha;
+
+  var movementX = (accelerationX + rotationRateBeta / 100) * 5; // Множитель для увеличения скорости движения по оси X
+  var movementY = (accelerationY + rotationRateGamma / 100) * 5; // Множитель для увеличения скорости движения по оси Y
+
+  xPosition += movementX;
+  yPosition += movementY;
+
+  // Ограничиваем движение по оси X до -100 и 100 пикселей
+  if (xPosition < -30) {
+    xPosition = -30;
+  } else if (xPosition > 30) {
+    xPosition = 30;
+  }
+
+  // Ограничиваем движение по оси Y до -100 и 100 пикселей
+  if (yPosition < -30) {
+    yPosition = -30;
+  } else if (yPosition > 30) {
+    yPosition = 30;
+  }
+
+  card.style.transform =
+    "translate3d(" + xPosition + "px, " + yPosition + "px, 0)";
+}
+let flag = true;
+
+function ready() {
+  if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
+    let card = document.getElementById("card");
+    if (flag) {
+      card.classList.add("heartBeat");
+    }
+    card.addEventListener("click", function () {
+      getAccel();
+    });
+  }
+
+  window.addEventListener("devicemotion", handleMotionEvent);
+}
+
+function getAccel() {
+  DeviceMotionEvent.requestPermission().then((response) => {
+    if (response == "granted") {
+      console.log("accelerometer permission granted");
+      flag = false;
+    }
+  });
+}
